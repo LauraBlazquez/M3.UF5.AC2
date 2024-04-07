30b57e45b04c8e4735b15b7fadd72726beabfc72
Laura Blázquez <laura.blazquez.7e7@itb.cat>
2024-04-07 22:12:37 +0200

    Add files via upload

---
+++ HelperClass.cs
@@ -0,0 +1,115 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+using CsvHelper;
+
+namespace AC2
+{
+    public static class HelperClass
+    {
+        public static List<Comarca> ReadCSV()
+        {
+            List<Comarca> comarques = new List<Comarca>();
+            using StreamReader reader = new StreamReader("consumAigua.csv");
+            using CsvReader csvreader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csvreader.Read();
+            csvreader.ReadHeader();
+            while (csvreader.Read())
+            {
+                var record = new Comarca
+                {
+                    Year = csvreader.GetField<int>("Any"),
+                    Code = csvreader.GetField<int>("Codi comarca"),
+                    Name = csvreader.GetField<string>("Comarca"),
+                    Population = csvreader.GetField<int>("Població"),
+                    DomesticExpense = csvreader.GetField<int>("Domèstic xarxa"),
+                    EconomicalActivitiesExpense = csvreader.GetField<int>(
+                        "Activitats econòmiques i fonts pròpies"
+                    ),
+                    Total = csvreader.GetField<int>("Total"),
+                    IndividualExpense = csvreader.GetField<decimal>("Consum domèstic per càpita")
+                };
+                comarques.Add(record);
+            }
+            return comarques;
+        }
+
+        public static void CreateXML(List<Comarca> comarques)
+        {
+            XDocument xmlDoc = new XDocument(
+                new XElement(
+                    "consum",
+                    from comarca in comarques
+                    select new XElement(
+                        "comarca",
+                        new XElement("Any", comarca.Year),
+                        new XElement("CodiComarca", comarca.Code),
+                        new XElement("NomComarca", comarca.Name),
+                        new XElement("Poblacio", comarca.Population),
+                        new XElement("DomesticXarxa", comarca.DomesticExpense),
+                        new XElement("ActivitatsEconomiques", comarca.EconomicalActivitiesExpense),
+                        new XElement("Total", comarca.Total),
+                        new XElement("ConsumDomesticPerCapita", comarca.IndividualExpense)
+                    )
+                )
+            );
+
+            string xmlFilePath = "consumAigua.xml";
+            xmlDoc.Save(xmlFilePath);
+        }
+
+        public static void PrintBigComarques(List<Comarca> comarques)
+        {
+            foreach (var comarca in comarques)
+            {
+                if (comarca.Population > 200000)
+                    Console.WriteLine(comarca.ToString());
+            }
+        }
+
+        public static void PrintDomesticAverageExpense(List<Comarca> comarques)
+        {
+            var avgByComarca = comarques
+                .GroupBy(c => c.Code)
+                .Select(g => new
+                {
+                    Code = g.Key,
+                    g.First().Name,
+                    AvgDomesticExpense = g.Average(c => c.DomesticExpense).ToString("0.00")
+                });
+
+            foreach (var comarca in avgByComarca)
+                Console.WriteLine(
+                    comarca.Code + " | " + comarca.Name + " | " + comarca.AvgDomesticExpense
+                );
+        }
+
+        public static List<Comarca> GetTop5Comarques(List<Comarca> comarques, bool ascending) /*Obtenir les 5 comarques amb el consum per càpita més alt o més baix*/
+        {
+            int maxYear = comarques.Max(c => c.Year);
+
+            var filteredComarques = comarques.Where(c => c.Year == maxYear);
+            return ascending
+                ? filteredComarques.OrderBy(c => c.IndividualExpense).Take(5).ToList()
+                : filteredComarques.OrderByDescending(c => c.IndividualExpense).Take(5).ToList();
+        }
+
+        public static void SearchComarca(List<Comarca> comarques, string comarca)
+        {
+            List<Comarca> comarcaToSearch = new List<Comarca>();
+            if (comarca.Length > 2)
+                comarcaToSearch = comarques.Where(c => c.Name == comarca).ToList();
+            else
+                comarcaToSearch = comarques.Where(c => c.Code == int.Parse(comarca)).ToList();
+
+            if (comarcaToSearch != null)
+            {
+                foreach (var c in comarcaToSearch)
+                    Console.WriteLine(c.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Comarca no trobada");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Xml.Linq;
+
+namespace AC2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string Menu = "Quines dades vols veure?\n1. Grans comarques (+200.000 habitants)\n2. Consum domèstic mitjà per comarca.\n3. Top 5 comarques amb consums per càpita més alts.\n4. Top 5 comarques amb consums per càpita més baixos.\n5. Cercar dades d'una comarca concreta.\n0. Sortir\n";
+            const string MSGName = "Introdueix el nom o el codi de la comarca que vols cerca: ";
+            const string Pause = "Prem una tecla per continuar...";
+            int option;
+            List<Comarca> comarques = HelperClass.ReadCSV();
+
+            try
+            {
+                XDocument.Load("consumAigua.xml");
+            }
+            catch
+            {
+                HelperClass.CreateXML(comarques);
+            }
+
+            do
+            {
+                Console.WriteLine(Menu);
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        HelperClass.PrintBigComarques(comarques);
+                        Console.WriteLine(Pause);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        HelperClass.PrintDomesticAverageExpense(comarques);
+                        Console.WriteLine(Pause);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        List<Comarca> filteredComarquesAsc = HelperClass.GetTop5Comarques(comarques,false);
+                        foreach (Comarca comarca in filteredComarquesAsc)
+                        {
+                            Console.WriteLine(comarca.ToString());
+                        }
+                        Console.WriteLine(Pause);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        List<Comarca> filteredComarquesDesc = HelperClass.GetTop5Comarques(comarques,true);
+                        foreach (Comarca comarca in filteredComarquesDesc)
+                        {
+                            Console.WriteLine(comarca.ToString());
+                        }
+                        Console.WriteLine(Pause);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.WriteLine(MSGName);
+                        string comarcaInput = Console.ReadLine();
+                        HelperClass.SearchComarca(comarques, comarcaInput);
+                        Console.WriteLine(Pause);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 0:
+                        Console.WriteLine("Chaito <3");
+                        break;
+                }
+            } while (option != 0);
+        }
+    }
+}
